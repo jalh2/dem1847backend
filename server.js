d77b87f8193af16737e8067aec7c9e648b15e20d
Dave jal <newagesocietyaltsoc@gmail.com>
2025-05-15 15:54:58 +0000

    c3

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
